c0558ef44cee6ddd49359111d9222bac555ef24d
GeyWhenHornie <adarshshukla221@outlook.com>
2025-04-19 21:09:07 +0530

    Update messages.ts

--- src/baileys/handlers/messages.ts
+++ src/baileys/handlers/messages.ts
@@ -21,8 +21,10 @@ export function messagesHandler({
 
         if (!metadata) return;
         if (metadata.msgType === 'unknown') return;
-        if (metadata.isGroup && metadata.groupMetaData.groupIsLocked) return;
         if (metadata.fromMe && ENV.IGNORE_SELF_MESSAGES) return;
+
+        // ✅ Removed this line to allow group messages
+        // if (metadata.isGroup && metadata.groupMetaData.groupIsLocked) return;
 
         await handleMessage({ client, msg, metadata, type });
       } catch (_) {}
